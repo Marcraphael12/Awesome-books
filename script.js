47e5264ee59871a51b6526f0db36fa391faa69f1
Marc Raphael <marcraphael979@gmail.com>
2021-09-20 09:25:07 +0100

    Create library elements

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const author = document.querySelector('.author');
+const book = document.querySelector('.book');
+const add = document.querySelector('.add');
+const here = document.querySelector('.here');
+const container = document.querySelector('.container');
 
+add.addEventListener('click', ev => {
+  const library = document.createElement('div');
+  const remove = document.createElement('button');
+  remove.setAttribute('type', 'button');
+  remove.innerHTML = 'delete';
+  remove.addEventListener('click', ev => {
+    container.removeChild(library);
+  });
+
+  library.innerHTML = `Book's author and name`;
+  library.appendChild(remove)
+  container.insertBefore(library, here);
+});
